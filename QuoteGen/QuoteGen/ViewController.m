cf96e5368a16a5cb88b9a812b0cae650b0f756de
Aniruddh Gokhale <gokhale.an@gmail.com>
2015-11-10 20:10:03 +0530

    Quotegen completed up to myQuotegeneration

--- QuoteGen/QuoteGen/ViewController.m
+++ QuoteGen/QuoteGen/ViewController.m
@@ -17,6 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.myQuotes=@[@"Live and let live",
+                    @"Don't cry over spilt milk",
+                    @"Always look on the bright side of life",
+                    @"Nobody's perfect",
+                    @"Can't see the woods for the trees",
+                    @"Better to have loved and lost then not loved at all",
+                    @"The early bird catches the worm",
+                    @"As slow as a wet week"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +33,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(IBAction)quoteButtonTapped:(id)sender{
+    
+    long array_tot=[self.myQuotes count];
+    
+    int index=(arc4random()% array_tot);
+    
+    
+    NSString *my_quote=self.myQuotes[index];
+    
+    self.quoteText.text=[NSString stringWithFormat:@"Quote:\n\n%@",my_quote];
+}
 @end
