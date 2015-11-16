03868e47d9f29365c823c58a223fd32b947f50ff
Aniruddh Gokhale <gokhale.an@gmail.com>
2015-11-16 14:02:52 +0530

    phase03-part02 started, Initial commit & "QuoteQuize project created" Ref:http://www.raywenderlich.com/31814/objectively-speaking-2-a-crash-course-in-objective-c-for-ios-6

---
+++ QuoteQuiz/QuoteQuiz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QuoteQuiz
+//
+//  Created by GDB Consultants on 16/11/15.
+//  Copyright (c) 2015 GDB Consultants. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
