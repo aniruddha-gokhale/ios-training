9c8d82a367a077486a476eaca9517b47c76c863b
Aniruddh Gokhale <gokhale.an@gmail.com>
2015-11-26 18:37:45 +0530

    phase06 (tut19) completed "Single and Multiple Selection in Collection View" Ref:http://www.appcoda.com/ios-collection-view-tutorial/

--- CollectionViewItemSelection/CollectionViewDemo/AppDelegate.h
+++ CollectionViewItemSelection/CollectionViewDemo/AppDelegate.h
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.h
-//  RecipePhoto
+//  CollectionViewDemo
 //
-//  Created by Simon on 13/1/13.
+//  Created by Simon on 9/1/13.
 //  Copyright (c) 2013 Appcoda. All rights reserved.
 //
 
--- CollectionViewItemSelection/CollectionViewDemo/RecipeCollectionViewController.h
+++ CollectionViewItemSelection/CollectionViewDemo/RecipeCollectionViewController.h
@@ -1,13 +1,15 @@
 //
 //  RecipeCollectionViewController.h
-//  RecipePhoto
+//  CollectionViewDemo
 //
-//  Created by Simon on 13/1/13.
+//  Created by Simon on 9/1/13.
 //  Copyright (c) 2013 Appcoda. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface RecipeCollectionViewController : UICollectionViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *shareButton;
+- (IBAction)shareButtonTouched:(id)sender;
 
 @end
