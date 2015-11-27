fafda51d5fd7180afe1754e11b0ee6025121e9d7
Aniruddh Gokhale <gokhale.an@gmail.com>
2015-11-27 13:16:47 +0530

    phase06 (tut21) completed "Create Static Table View Using Storyboard" Ref:http://www.appcoda.com/ios-static-table-view-storyboard/

---
+++ StaticTableDemo/StaticTableDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticTableDemo
+//
+//  Created by GDB Consultants on 27/11/15.
+//  Copyright (c) 2015 GDB Consultants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
