6584bfa1c44a647a23a53cac290f82652cad525d
Aniruddh Gokhale <gokhale.an@gmail.com>
2015-12-01 19:28:33 +0530

    phase06 (tut 27) completed "Add a Slide-out Sidebar Menu in Your Apps" Ref:http://www.appcoda.com/ios-programming-sidebar-navigation-menu/

---
+++ SidebarDemoTemplate/SidebarDemo/MainViewController.h
@@ -0,0 +1,8 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+
+@end
---
+++ SidebarDemoTemplate/SidebarDemo/MainViewController.m
@@ -0,0 +1,35 @@
+
+#import "MainViewController.h"
+#import "SWRevealViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.title = @"News";
+    
+    SWRevealViewController *revealViewController= self.revealViewController;
+    
+    if(revealViewController){
+        
+        [self.sidebarButton setTarget:self.revealViewController];
+        [self.sidebarButton setAction:@selector(revealToggle: )];
+        [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+        
+    }
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
